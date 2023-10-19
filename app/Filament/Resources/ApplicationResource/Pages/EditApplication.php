f8d25bab54f41be543f93c57d6b9345b85b6388e
Lucas <lucas.breiner@gmx.de>
2023-10-19 18:25:12 +0200

    add redirect to edit page

--- app/Filament/Resources/ApplicationResource/Pages/EditApplication.php
+++ app/Filament/Resources/ApplicationResource/Pages/EditApplication.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ApplicationResource\Pages;
 use App\Filament\Resources\ApplicationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
+
 
 class EditApplication extends EditRecord
 {
@@ -15,5 +17,17 @@ class EditApplication extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Application updated')
+            ->body('The Application has bin saved successfully.');
     }
 }

3cdaa44c9a357dda649546f4d32c5f9728a03dca
Lucas <lucas.breiner@gmx.de>
2023-10-21 13:46:04 +0200

    create layout for companies

--- app/Filament/Resources/CompanyResource.php
+++ app/Filament/Resources/CompanyResource.php
@@ -5,13 +5,14 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CompanyResource\Pages;
 use App\Filament\Resources\CompanyResource\RelationManagers;
 use App\Models\Company;
-use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class CompanyResource extends Resource
 {
@@ -23,7 +24,26 @@ class CompanyResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->string()
+                    ->required(),
+                TextInput::make('url')
+                    ->string()
+                    ->required(),
+                TextInput::make('street')
+                    ->string()
+                    ->required(),
+                TextInput::make('zip')
+                    ->string()
+                    ->required(),
+                TextInput::make('travel_time_train')
+                    ->label('Öffi Travel Time (minutes)')
+                    ->string(),
+                TextInput::make('travel_time_car')
+                    ->label('Car Travel Time (minutes)')
+                    ->string(),
+                TextInput::make('company_type')
+                    ->string()
             ]);
     }
 
@@ -31,7 +51,30 @@ class CompanyResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Split::make([
+                    TextColumn::make('name')
+                        ->label('Name')
+                        ->searchable()
+                        ->sortable(),
+                    Stack::make([
+                        TextColumn::make('url')
+                            ->icon('heroicon-m-globe-europe-africa'),
+                        TextColumn::make('street')
+                            ->default('no street yet'),
+                        TextColumn::make('zip')
+                            ->default('no zip yet'),
+                    ]),
+                    Stack::make([
+                        TextColumn::make('travel_time_train')
+                            ->sortable()
+                            ->icon('phosphor-train')
+                            ->default('missing'),
+                        TextColumn::make('travel_time_car')
+                            ->sortable()
+                            ->icon('phosphor-car')
+                            ->default('missing')
+                    ]),
+                ])
             ])
             ->filters([
                 //
